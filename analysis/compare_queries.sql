f699d1902de9582acc7dad7d7f03785249d80957
axpe.atr@gmail.com <axpe.atr@gmail.com>
2025-07-25 12:18:19 +0000

    AUDITING_2

--- analysis/compare_queries.sql
+++ analysis/compare_queries.sql
@@ -1,7 +1,7 @@
 
 
-{% set old_etl_relation=ref{'customer_orders'} %}
-{% set dbt_relation=ref{'fct_customer_orders'} %}
+{% set old_etl_relation=ref('customer_orders') %}
+{% set dbt_relation=ref('fct_customer_orders') %}
 
 {{ audit_helper.compare_relations(
     a_relation = old_etl_relation,
